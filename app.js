9c6ac15e61bc57565701a32ae6f56dc8fa34901c
Jason Poindexter <poindexter.json@gmail.com>
2018-07-27 14:50:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@
 // fs.createReadStream(__dirname+'/2018 - I, Robot-2018-07-27.csv').pipe(parser);
 
 
-const csvFilePath='./2018 - I, Robot-2018-07-27.csv';
+const csvFilePath='./2018 - I, Robot-2018-07-27.csv'; //TODO Fetch this directly from babalooey
 const csv=require('csvtojson');
 csv()
     .fromFile(csvFilePath)
