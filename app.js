25fb533243468eccb45aa274394528d5053edb5a
Jason Poindexter <jason@launchbadge.com>
2018-07-27 14:39:22 -0700

    Initial commit. Parse shifts csv to json object.

---
+++ app.js
@@ -0,0 +1,37 @@
+/* ----- outputs array of csv cells ---- */
+// const fs = require('fs');
+// const parse = require('csv-parse');
+
+// function groupBy(collection, property) {
+//     var i = 0, val, index,
+//         values = [], result = [];
+//     for (; i < collection.length; i++) {
+//         val = collection[i][property];
+//         index = values.indexOf(val);
+//         if (index > -1)
+//             result[index].push(collection[i]);
+//         else {
+//             values.push(val);
+//             result.push([collection[i]]);
+//         }
+//     }
+//     return result;
+// }
+//
+// const parser = parse({relax_column_count: true}, function (err, shifts) {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(shifts);
+// });
+//
+// fs.createReadStream(__dirname+'/2018 - I, Robot-2018-07-27.csv').pipe(parser);
+
+
+const csvFilePath='./2018 - I, Robot-2018-07-27.csv';
+const csv=require('csvtojson');
+csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj)=>{
+        console.log(jsonObj);
+    });
