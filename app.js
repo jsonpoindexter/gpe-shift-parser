09765d71250084e159106803d96b143302587058
Jason Poindexter <jason@launchbadge.com>
2018-07-27 15:45:23 -0700

    Group shifts into an array by User Id

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const csvFilePath='./2018 - I, Robot-2018-07-27.csv'; //TODO Fetch this directly from babalooey
-const csv=require('csvtojson');
+const csvFilePath = './2018 - I, Robot-2018-07-27.csv'; //TODO Fetch this directly from babalooey
+const csv = require('csvtojson');
+
+function groupBy(collection, property) {
+    var i = 0, val, index,
+        values = [], result = [];
+    for (; i < collection.length; i++) {
+        val = collection[i][property];
+        index = values.indexOf(val);
+        if (index > -1)
+            result[index].push(collection[i]);
+        else {
+            values.push(val);
+            result.push([collection[i]]);
+        }
+    }
+    return result;
+}
+
 csv()
     .fromFile(csvFilePath)
-    .then((jsonObj)=>{
-        console.log(jsonObj);
+    .then((shifts)=>{
+        const shift = shifts[0]
+        // console.log(shift[])
+        console.log(groupBy(shifts, "User ID")[0]);
     });
