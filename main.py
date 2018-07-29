b77a753e25f82f9ca0ca3807354b352287a62b98
Jason Poindexter <poindexter.json@gmail.com>
2018-07-28 20:09:10 -0700

    Group shifts by User Id

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import csv
+# from collections import defaultdict
+import itertools
+from operator import itemgetter
+
+main_event_start = '2018-08-26 00:00'
 
 reader = csv.DictReader(open('2018 - I, Robot-2018-07-27.csv'))
 
-dict_list = []
+shifts = []
+for shift in reader:
+    if shift['User ID'] is not None and shift['User ID'].strip() is not "":
+        shifts.append(shift)
 
-for line in reader:
-    dict_list.append(line)
+shifts = sorted(shifts, key=itemgetter('User ID'))
 
-print(dict_list[0])
+grouped_shifts = []
+for key, value in itertools.groupby(shifts, key=itemgetter('User ID')):
+    user_shifts = []
+    for user_shift in value:
+        user_shifts.append(user_shift)
+    grouped_shifts.append(user_shifts)
