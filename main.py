d765fd8e6e919f95a3eddfe0e61c9133cbc0b817
Jason Poindexter <poindexter.json@gmail.com>
2019-07-04 11:47:33 -0700

    Rule Updates for 2019
    - Update event_id 31 -> 38
    -  Modify dates + 1 year, -1 day
    - Update trainee refresh and bar role IDs

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import wapstatus
 from datetime import datetime
 
-event_id = 31
+event_id = 38
 
-main_event_start = datetime.strptime('2018-08-26 00:00', '%Y-%m-%d %H:%M')  # Main event start shift date/time
-main_start_event_end = datetime.strptime('2018-08-27 00:00', '%Y-%m-%d %H:%M')
-earliest_wap_date = datetime.strptime('2018-08-17 00:00',
+main_event_start = datetime.strptime('2019-08-25 00:00', '%Y-%m-%d %H:%M')  # Main event start shift date/time
+main_start_event_end = datetime.strptime('2019-08-26 00:00', '%Y-%m-%d %H:%M')
+earliest_wap_date = datetime.strptime('2019-08-16 00:00',
                                       '%Y-%m-%d %H:%M')  # Earliest date/time that a WAP can be given
-day_off_date = datetime.strptime('2018-08-23 00:00',
+day_off_date = datetime.strptime('2019-08-22 00:00',
                                  '%Y-%m-%d %H:%M')  # If your first day working is before the 23rd you may take a day off during pre-event.
-train_r_role_id = '1755'  # User WAP/Credits should only count 1 training (even if 1+ scheduled)
-bar_role_id = '1752'
+train_r_role_id = '2200'  # User WAP/Credits should only count 1 training (even if 1+ scheduled)
+bar_role_id = '2184'
 
 wapstatus = wapstatus.WapStatus(
     event_id,
