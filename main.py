59be2c996316491a430f544f362d2a2c28167452
NatashaGayte <91878257+NatashaGayte@users.noreply.github.com>
2022-06-24 23:36:48 -0700

    Update main.py
    Added dates for 2022 and shift ID's

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 event_id = 38
 
-main_event_start = datetime.strptime('2019-08-25 00:00', '%Y-%m-%d %H:%M')  # Main event start shift date/time
-main_start_event_end = datetime.strptime('2019-08-26 00:00', '%Y-%m-%d %H:%M')
-earliest_wap_date = datetime.strptime('2019-08-16 00:00',
+main_event_start = datetime.strptime('2022-08-15 00:00', '%Y-%m-%d %H:%M')  # Main event start shift date/time
+main_start_event_end = datetime.strptime('2022-08-27 23:00', '%Y-%m-%d %H:%M')
+earliest_wap_date = datetime.strptime('2022-08-16 00:00',
                                       '%Y-%m-%d %H:%M')  # Earliest date/time that a WAP can be given
-day_off_date = datetime.strptime('2019-08-22 00:00',
-                                 '%Y-%m-%d %H:%M')  # If your first day working is before the 23rd you may take a day off during pre-event.
-train_r_role_id = '2200'  # User WAP/Credits should only count 1 training (even if 1+ scheduled)
-bar_role_id = '2184'
+day_off_date = datetime.strptime('2022-08-25 00:00',
+                                 '%Y-%m-%d %H:%M')  # If your first day working is before the 25rd you may take a day off during pre-event.
+train_r_role_id = '2745'  # User WAP/Credits should only count 1 training (even if 1+ scheduled)
+bar_role_id = '2726'
 parent_ids = ['1VJvc4riiDHdTDQhVGRGlQKH93DxQwOG6'] # Google Drive Parent folder ID
 
 wapstatus = wapstatus.WapStatus(
